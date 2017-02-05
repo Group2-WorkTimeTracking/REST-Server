c41d671add90b41709fd4ef5c9f364f86a2b7dc9
Benjamin Collet <benjacollet@gmail.com>
2017-02-05 19:33:09 +0200

    added objects

--- app.py
+++ app.py
@@ -1,51 +1,44 @@
 import os
 from flask import Flask, request, redirect
 
+from Location import *
+from Log import *
+
+
 app = Flask(__name__)
 
-o_account = """{...}"""
 
-o_location = """{
-    "placeName": "OAMK, Kotkantie campus",
-    "coordinate": {"latitude": 64.99958,
-                   "longitude": 25.51078},
-    "size": 0.00220
-}"""
+acc = """{...}"""
 
-o_logs = """{
+loc = Location('OAMK, Kotkantie campus', 64.99958, 25.51078, 0.00220)
+
+lgs = """{
     "month": "2016-12",
-    "total-working-hours": 142.1,
-    "daily-work": [
+    "totalWorkingHours": 142.1,
+    "dailyWork": [
         {
             "day": "2016-12-01",
-            "working-hours": 7.9,
+            "workingHours": 7.9,
             "note": ""
         },
         {
             "day": "2016-12-02",
-            "working-hours": 8.3,
+            "workingHours": 8.3,
             "note": ""
         },
         {
             "day": "2016-12-05",
-            "working-hours": 0.0,
+            "workingHours": 0.0,
             "note": "I have a meeting in Helsinki."
         },
         ...
     ]
 }"""
 
-o_timetag = """{
-    "type": "start",
-    "timestamp": 1485096644,
-    "user-id": 42
-}"""
+timetag = TimeTag('start', 1485096644, 42)
 
-o_note = """{
-    "message": "I won’t be present because of a meeting in Helsinki.",
-    "day-of-effectiveness": "2017-01-22",
-    "user-id": 42
-}"""
+msg = Note("I won't be present because of a meeting in Helsinki.",
+    '2017-01-22', 42)
 
 
 @app.route('/')
@@ -55,31 +48,31 @@ def index():
 
 @app.route('/login', methods=['POST'])
 def login():
-    return o_account
+    return acc
 
 
 @app.route('/account', methods=['GET', 'PUT'])
 def account():
     if request.method == 'GET':
-        return o_account
+        return acc
     else:
-        return o_account
+        return acc
 
 
 @app.route('/employee', methods=['GET', 'POST'])
 def employee_account():
     if request.method == 'GET':
-        return '[{}]'.format(o_account)
+        return acc
     else:
-        return o_account
+        return acc
 
 
 @app.route('/employee/<int:employee_id>', methods=['GET', 'PUT', 'DELETE'])
 def employee_account_by_id(employee_id):
     if request.method == 'GET':
-        return o_account
+        return acc
     elif request.method == 'PUT':
-        return o_account
+        return acc
     else:
         return '', 204
 
@@ -87,17 +80,17 @@ def employee_account_by_id(employee_id):
 @app.route('/location', methods=['GET', 'POST'])
 def location():
     if request.method == 'GET':
-        return '{}'.format(o_location)
+        return repr(loc)
     else:
-        return o_location
+        return repr(loc)
 
 
 @app.route('/location/<int:location_id>', methods=['GET', 'PUT', 'DELETE'])
 def location_by_id(location_id):
     if request.method == 'GET':
-        return o_location
+        return repr(loc)
     elif request.method == 'PUT':
-        return o_location
+        return repr(loc)
     else:
         return '', 204
 
@@ -105,49 +98,49 @@ def location_by_id(location_id):
 @app.route('/employee/<int:employee_id>/location', methods=['GET', 'PUT'])
 def employee_location(employee_id):
     if request.method == 'GET':
-        return o_location
+        return repr(loc)
     elif request.method == 'PUT':
-        return o_location
+        return repr(loc)
 
 
 @app.route('/employee/<int:employee_id>/logs', methods=['GET'])
 def employee_logs(employee_id):
-    return o_logs
+    return lgs
 
 
 @app.route('/employee/<int:employee_id>/logs/<int:month_id>', methods=['GET'])
 def employee_logs_per_month(employee_id, month_id):
-    return o_logs
+    return lgs
 
 
 @app.route('/account/location', methods=['GET'])
 def account_location():
-    return o_location
+    return repr(loc)
 
 
 @app.route('/work/start', methods=['POST'])
 def start():
-    return o_timetag
+    return repr(timetag)
 
 
 @app.route('/work/finish', methods=['POST'])
 def finish():
-    return o_timetag
+    return repr(timetag)
 
 
 @app.route('/work/note', methods=['POST'])
 def note():
-    return o_note
+    return repr(msg)
 
 
 @app.route('/account/logs', methods=['GET'])
 def logs():
-    return o_logs
+    return lgs
 
 
 @app.route('/account/logs/<int:month_id>', methods=['GET'])
 def logs_per_month(month_id):
-    return o_logs
+    return lgs
 
 
 if __name__ == '__main__':
